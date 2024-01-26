e8abbb3cb9ef1934aab7db9d9e0bcdd9722b3332
Dimi Tsonev <dconev@gmail.com>
2024-01-26 19:54:22 +0000

    created routes

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import Home from './components/Home'
+import Project from './components/Project'
+import Contact from './components/Contact'
+import ProjectGallery from './components/ProjectGallery';
+
+
 
 function App() {
-
-
   return (
-  <>div</>
-  )
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+        <Route path="/projects" element={<ProjectGallery/>}/>
+        <Route path="/project/:id" element={<Project/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- portfolio/src/components/ProjectGalary.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProjectGalary = () => {
-  return (
-    <div>ProjectGalary</div>
-  )
-}
-
-export default ProjectGalary
